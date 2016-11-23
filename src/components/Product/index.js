a5601f257008b424cba87ad76d8f6e9b8e930b83
Emanuel Larini <emanuel@jp7.com.br>
2016-11-23 19:10:22 -0200

    Fix close button css

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 const styles = {
   close: {
     position: "absolute",
-    right: 5,
-    top: -12,
+    marginRight: -15,
+    marginTop: -15,
+    right: 0,
+    top: 0,
     border: '5px solid transparent',
     borderRadius: 50,
     backgroundColor: 'red',
-    color: 'white'
+    color: 'white',
+  },
+  thumbnail: {
+    position: 'relative'
   }
 }
 
@@ -19,7 +24,7 @@ class Product extends Component {
   }
   render() {
     return (
-      <div className="thumbnail well text-center clearfix">
+      <div className="thumbnail well text-center clearfix" style={styles.thumbnail}>
         <button style={styles.close}>x</button>
         <img src={this.product.img} alt="asdasdasd"/>
         <div className="caption">
@@ -27,7 +32,7 @@ class Product extends Component {
           <p>{this.product.description}</p>
         </div>
 
-        <button className="btn btn-primary pull-right ">Salvar</button>
+        <button className="btn btn-primary pull-right">Salvar</button>
       </div>
     );
   }
